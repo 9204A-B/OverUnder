29880215c35d1e84090ccf986c2dda70f78acc66
coollogan876 <119338946+coollogan876@users.noreply.github.com>
2023-11-03 12:12:08 -0500

    Update main.py

--- Arcade Drive/src/main.py
+++ Arcade Drive/src/main.py
@@ -90,7 +90,7 @@ rc_auto_loop_thread_controller_1 = Thread(rc_auto_loop_function_controller_1)
 # ------------------------------------------
 # 
 # 	Project:      OverUnder Arcade
-#	Author:       VEX
+#	Author:       Logan Dresel and Daniel Milan
 #	Created:
 #	Description:  VEXcode V5 Python Project
 # 
